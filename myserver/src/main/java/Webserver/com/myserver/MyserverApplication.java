7ce93367587e9ef0bef23b2f2447fe90cbc2f52d
trankhiem <trankhiem2003@gmail.com>
2023-11-21 22:38:00 +0700

    update(add):InitRunableProject

---
+++ myserver/src/main/java/Webserver/com/myserver/MyserverApplication.java
@@ -0,0 +1,13 @@
+package Webserver.com.myserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyserverApplication.class, args);
+	}
+
+}
---
+++ myserver/src/test/java/Webserver/com/myserver/MyserverApplicationTests.java
@@ -0,0 +1,13 @@
+package Webserver.com.myserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
