5a1a65c38e22927a29b7d24a69c113d2519b8517
trankhiem <trankhiem2003@gmail.com>
2023-12-20 14:12:11 +0700

    update(add):CompleteForgetPassFlow

--- myserver/src/main/java/Webserver/com/myserver/HelperFunction/IDgenerator.java
+++ myserver/src/main/java/Webserver/com/myserver/HelperFunction/IDgenerator.java
@@ -2,6 +2,7 @@ package Webserver.com.myserver.HelperFunction;
 
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
+import java.util.Random;
 
 public class IDgenerator {
     public static String GenId(String input){
@@ -14,6 +15,19 @@ public class IDgenerator {
 
     public static synchronized int getAutoIncrementedNumber() {
         return ++callCounter;
+    }
+    private static final String CHARACTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+    public static String GenPass(int length){
+        Random random = new Random();
+        StringBuilder sb = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(CHARACTERS.length());
+            char randomChar = CHARACTERS.charAt(randomIndex);
+            sb.append(randomChar);
+        }
+
+        return sb.toString();
     }
 
 }
